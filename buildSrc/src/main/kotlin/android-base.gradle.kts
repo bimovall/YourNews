3efe595b8b066cdda6c4100de135e5541678da36
bimovall <bimovallentino@gmail.com>
2020-09-22 14:56:42 +0700

    - fix github workflow 1

--- buildSrc/src/main/kotlin/android-base.gradle.kts
+++ buildSrc/src/main/kotlin/android-base.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     kotlin("android")
     kotlin("android.extensions")
     kotlin("kapt")
-    id("dagger.hilt.android.plugin")
-    id("androidx.navigation.safeargs.kotlin")
+    //id("dagger.hilt.android.plugin")
+    //id("androidx.navigation.safeargs.kotlin")
 }
+apply(plugin  = "dagger.hilt.android.plugin")
+apply(plugin  = "androidx.navigation.safeargs.kotlin")
